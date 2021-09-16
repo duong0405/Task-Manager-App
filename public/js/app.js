f1b576a8758643d98285f55c60a14d45630dec6a
Phuong Duong <phuongx31@gmail.com>
2021-09-16 14:32:27 +0700

    add form

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,28 @@
 console.log("Client side javascript file is loaded!");
 
-fetch("http://localhost:3000/weather?address=Hanoi").then(response => {
-    response.json().then(data => {
-        if (data.error) {
-            console.log(data.error);
-        } else {
-            console.log(data);
-        }
+
+const weatherForm = document.querySelector("form")
+const search = document.querySelector("input")
+const messageOne = document.getElementById("message-1")
+const messageTwo = document.getElementById("message-2")
+
+weatherForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const location = search.value
+    
+    messageOne.textContent = "Loading..."
+    messageTwo.textContent = ""
+
+    fetch("http://localhost:3000/weather?address=" + location).then(response => {
+        response.json().then(data => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                console.log(data);
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecase
+            }
+        })
     })
 })
