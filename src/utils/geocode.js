b90258a237d4f95fc87a3b8931c97e9f84969961
Phuong Duong <phuongx31@gmail.com>
2021-09-16 12:31:03 +0700

    fix error when invalid address

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,10 +3,14 @@ const request = require("request");
 const geocode = (address, callback) => {
 	const url = "http://api.positionstack.com/v1/forward?access_key=e3ba2d3920324732669b15df68cccfab&query=" + encodeURIComponent(address) + "&limit=1";
 
+	console.log(url);
+
 	request({ url, json: true }, (error, {body}) => {
+		console.log(error);
+		console.log(body);
 		if (error) {
 			callback("Unable to connect to location services!", undefined);
-		} else if (body.error) {
+		} else if (body.data.length === 0) {
 			callback("Unable to find location. Try another search.", undefined);
 		} else {
 			callback(undefined, {
