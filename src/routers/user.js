78e212512e3082780002996c9f7cf6ab2915cc91
Phuong Duong <phuongx31@gmail.com>
2021-09-29 16:46:18 +0700

    adding API authentication and hashsing password

--- src/routers/user.js
+++ src/routers/user.js
@@ -13,6 +13,15 @@ router.post('/users', async (req, res) => {
     }
 })
 
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
 router.get('/users', async (req, res) => {
 
     try {
@@ -48,7 +57,11 @@ router.patch('/users/:id', async (req, res) => {
 
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        const user = await User.findById(req.params.id)
+        updates.forEach((update) => user[update] = req.body[update])
+
+        await user.save()
+        // const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
 
         if (!user) {
             return res.status(404).send()
@@ -56,7 +69,7 @@ router.patch('/users/:id', async (req, res) => {
 
         res.send(user)
     } catch (e) {
-        res.status(400).send()
+        res.status(400).send(e)
     }
 })
 
