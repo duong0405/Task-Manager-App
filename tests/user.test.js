fa946a77491e80f00f119a39fd2b8d1c464443bc
Phuong Duong <phuongx31@gmail.com>
2021-10-22 15:36:48 +0700

    testing with user authentication

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,11 +1,19 @@
 const request = require('supertest')
-const app = require('../src/app')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 const User = require('../src/models/user')
 
+const app = require('../src/app')
+
+const userOneID = new mongoose.Types.ObjectId()
 const userOne = {
+    _id: userOneID,
     name: 'Mike',
     email: 'mike@example.com',
-    password: 'what!124bg'
+    password: 'what!124bg',
+    tokens: [{
+        token: jwt.sign({ _id: userOneID }, process.env.JWT_SECRET)
+    }]
 }
 
 beforeEach(async () => {
@@ -33,4 +41,34 @@ test('Login failure', async () => {
         email: 'notexist@example.com',
         password: '123445pass!'
     }).expect(400)
+})
+
+test('Get user profile', async () => {
+    await request(app)
+        .get('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+})
+
+test('Get user profile for unauthenticated user', async () => {
+    await request(app)
+        .get('/users/me')
+        .send()
+        .expect(401)
+})
+
+test('Delete user profile', async () => {
+    await request(app)
+        .delete('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+})
+
+test('Delete user profile for unauthenticated user', async () => {
+    await request(app)
+        .delete('/users/me')
+        .send()
+        .expect(401)
 })
